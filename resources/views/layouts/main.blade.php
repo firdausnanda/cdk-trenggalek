e34c8d31664b3cd85b7c9dc30c323861e2121c42
firdausnanda <firdausnanda46@gmail.com>
2025-07-30 17:25:07 +0700

    [FIX] Add jquery

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,8 +47,7 @@
     @include('layouts.partials.footer')
 
     <!-- Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="{{ asset('vendor/jquery/jquery-3.7.1.min.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/share.js') }}"></script>
 
