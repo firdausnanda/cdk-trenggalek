055f52d1eeb1f1b49e0385597bf774df43af831a
firdausnanda <firdausnanda46@gmail.com>
2025-07-31 09:56:05 +0700

    [FIX] logviewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -64,5 +64,10 @@ class AppServiceProvider extends ServiceProvider
         LogViewer::auth(function ($request) {
             return Auth::check() && Auth::user()->hasRole('Superadmin');
         });
+
+        // Only allowed people can view the settings.
+        Gate::define('viewLogViewer', function (User $user) {
+            return $user->hasRole('Superadmin');
+        });
     }
 }
